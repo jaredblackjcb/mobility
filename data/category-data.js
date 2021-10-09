7f2e6db705e0a19b19a0a7caae739063bc25adcf
Jared Black <jaredblackjcb@gmail.com>
2021-10-09 10:44:07 -0500

    Create Category data

---
+++ data/category-data.js
@@ -0,0 +1,11 @@
+import WorkoutCategory from '../models/WorkoutCategory';
+
+export const WORKOUT_CATEGORIES = [
+    new WorkoutCategory(100, "Neck", "test"),
+    new WorkoutCategory(101, "Back", "test"),
+    new WorkoutCategory(102, "Shoulders", "test"),
+    new WorkoutCategory(103, "Hips", "test"),
+    new WorkoutCategory(104, "Knees", "test"),
+    new WorkoutCategory(105, "Ankles", "test"),
+    new WorkoutCategory(106, "Wrists", "test")
+];
