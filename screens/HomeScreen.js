de9473ec1b24e4bba9a63e487969e37533194785
Jared Black <jaredblackjcb@gmail.com>
2022-02-01 09:00:00 -0600

    Send 3 randomly selected categoryIDs to the Exercise screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 import RadioButtonDuration from '../components/RadioButtonDuration';
+import { WORKOUT_CATEGORIES } from '../data/app-data';
+
 
 export default function HomeScreen({navigation}) {
+  const selectedIDs = () => {
+    let catIDs = [];
+    for (let category of WORKOUT_CATEGORIES) {
+      catIDs.push(category.id);
+    }
+    console.log("catIDs: " + catIDs);
+    return getRandomSlice(catIDs, 3);
+  }
+  
+  function getRandomSlice(array, n) {
+    const shuffled = array.sort(() => 0.5 - Math.random());
+    const selected = shuffled.slice(0, n);
+    console.log("Selected IDs: " + selected);
+    return selected;
+  }
+
     return (
       <View style={styles.container}>
         <View style={styles.imageContainer}>
-        <TouchableOpacity onPress={() => {navigation.navigate('Workout')}} style={styles.imageTouchable}>
+        <TouchableOpacity onPress={() => {navigation.navigate('Workout', {categoryIDs: selectedIDs()})}} style={styles.imageTouchable}>
           <ImageBackground source={require('../assets/home_screen/home1.jpg')} resizeMode='cover' style={styles.image}>
             <Text style={styles.h1Text}>Full Body Session</Text>
           </ImageBackground>
