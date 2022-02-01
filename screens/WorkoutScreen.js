d8682a2aca4d41b58bc9236f3da81a6c48be2312
Jared Black <jaredblackjcb@gmail.com>
2022-02-01 08:06:42 -0600

    Add exercise filtering based on selected categories

--- screens/WorkoutScreen.js
+++ screens/WorkoutScreen.js
@@ -3,16 +3,38 @@ import { ScrollView, View, Text, StyleSheet } from 'react-native';
 import Accordion from '../components/Accordion';
 import { EXERCISES } from '../data/app-data';
 import Colors from '../constants/Colors';
+import Exercise from '../models/Exercise';
 
 // const categoryIds = props.navigation.getParam('categoryIds');
 // const selectedExercises = EXERCISES.find(exercise => exercise.categoryIds = categoryIds); //TODO: Just an example. Need to figure out how to select 3-5 exercises that have selected categoryIds from the selection screen in their categoryId list
 
 
-export default function SelectCategoryScreen() {
+export default function WorkoutScreen({route, navigation}) {
+  const categoryIDs = route.params.categoryIDs;
+
+  const filteredExercises = catIDs => {
+    const filteredExercises = [];
+    // Pass in EXERCISES
+    for (let exercise of EXERCISES) {
+      const ids = exercise.categoryIds.categoryIDs;
+      for (let id of ids){
+        if (catIDs.includes(id) && !filteredExercises.includes(exercise)) {
+          filteredExercises.push(exercise);
+        }
+      }
+      console.log(ids);
+      }
+    for (let i = 0; i < catIDs.length; i++) {
+      console.log(catIDs[i]);
+    }
+    return filteredExercises;
+  }
+
+
     return (
       <View style={styles.container}>
       <ScrollView>
-          {renderAccordions(EXERCISES)}
+          {renderAccordions(filteredExercises(categoryIDs))}
       </ScrollView>
         </View>
     );
@@ -20,9 +42,9 @@ export default function SelectCategoryScreen() {
 
   const renderAccordions = (data) => {
     const accordions = [];
-    for (item of data) {
+    for (let item of data) {
       accordions.push(
-        <Accordion 
+        <Accordion
           title={item.title}
           description={item.description}
           imagePath={item.imagePath}
