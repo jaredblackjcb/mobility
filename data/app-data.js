c916ddc214a9ee567a2751f9f82ff6cb8d43be42
Jared Black <jaredblackjcb@gmail.com>
2022-02-01 08:04:42 -0600

    Modify data format to be accessible from the exercise screen

--- data/app-data.js
+++ data/app-data.js
@@ -12,8 +12,8 @@ export const WORKOUT_CATEGORIES = [
 ];
 
 export const EXERCISES = [
-    new Exercise(200, [101, 103], "Plank", "Keep your body straight and your neck parallel with the floor. Hold for as long as you can or until time is up.", require("../assets/exercise/placeholder.jpeg")),
-    new Exercise(201, [105], "Ankle Rolls", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum!", require("../assets/exercise/placeholder.jpeg")),
-    new Exercise(202, [103, 104, 105], "Deep Squat", "Provident similique accusantium nemo autem Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!", require("../assets/exercise/placeholder.jpeg")),
-    new Exercise(203, [101, 106], "Cobra", "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam ecusandae alias error harum maxime adipisci amet laborum.", require("../assets/exercise/placeholder.jpeg"))    
+    new Exercise(200, {categoryIDs: [101, 103]}, "Plank", "Keep your body straight and your neck parallel with the floor. Hold for as long as you can or until time is up.", require("../assets/exercise/placeholder.jpeg")),
+    new Exercise(201, {categoryIDs: [105]}, "Ankle Rolls", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum!", require("../assets/exercise/placeholder.jpeg")),
+    new Exercise(202, {categoryIDs: [103, 104, 105]}, "Deep Squat", "Provident similique accusantium nemo autem Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!", require("../assets/exercise/placeholder.jpeg")),
+    new Exercise(203, {categoryIDs: [100, 101, 102, 103, 106]}, "Cobra", "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam ecusandae alias error harum maxime adipisci amet laborum.", require("../assets/exercise/placeholder.jpeg"))    
 ];
