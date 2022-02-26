bb5ef1f575cf90c0485668e14abaaa200ee369ef
Jared Black <jaredblackjcb@gmail.com>
2022-02-26 13:42:21 -0600

    Add method to display only 3 exercises with at least one from each selected category

--- screens/WorkoutScreen.js
+++ screens/WorkoutScreen.js
@@ -4,72 +4,74 @@ import Accordion from '../components/Accordion';
 import { EXERCISES } from '../data/app-data';
 import Colors from '../constants/Colors';
 
-export default function WorkoutScreen({route, navigation}) {
+export default function WorkoutScreen({ route, navigation }) {
   const categoryIDs = route.params.categoryIDs;
 
+  // Return 3 exercises. Include at least one exercise from each category contained in catIDs. 
   const filteredExercises = catIDs => {
-    const filteredExercises = [];
-    // Pass in EXERCISES
-    for (let exercise of EXERCISES) {
-      const ids = exercise.categoryIds.categoryIDs;
-      for (let id of ids){
-        if (catIDs.includes(id) && !filteredExercises.includes(exercise)) {
-          filteredExercises.push(exercise);
+    let selectedExercises = [];
+    // console.log("catIDs: " + catIDs);
+    while (selectedExercises.length < 3) {
+      for (let catID of catIDs) {
+        const categoryExercises = EXERCISES.filter(exercise => exercise.categoryIds.categoryIDs.includes(catID));
+        const selectedExercise = categoryExercises[Math.floor(Math.random() * categoryExercises.length)];
+        // console.log("selectedExercise: " + JSON.stringify(selectedExercise.categoryIds));
+        // console.log("catID: " + catID);
+        if (selectedExercises.length < 3 && !selectedExercises.includes(selectedExercise)) {
+          selectedExercises.push(selectedExercise);
         }
+        // console.log("selectedExercises: " + JSON.stringify(selectedExercises));
       }
-      // console.log(ids);
-      }
-    // for (let i = 0; i < catIDs.length; i++) {
-      // console.log(catIDs[i]);
-    // }
-    return filteredExercises;
-  }
-
-
-    return (
-      <View style={styles.container}>
-      <ScrollView>
-          {renderAccordions(filteredExercises(categoryIDs))}
-          <TouchableOpacity style={styles.endButton} onPress={() => {navigation.navigate('Home')}}>
-            <Text style={styles.buttonTitle}>Finish</Text>
-          </TouchableOpacity>
-      </ScrollView>
-        </View>
-    );
-  }
-
-  const renderAccordions = (data) => {
-    const accordions = [];
-    for (let item of data) {
-      accordions.push(
-        <Accordion
-          title={item.title}
-          description={item.description}
-          imagePath={item.imagePath}
-        />
-      )
     }
-    return accordions;
+    return selectedExercises;
   }
 
-  const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        paddingTop: 30,
-        backgroundColor: Colors.backgroundColor
-      },
-      endButton: {
-        marginTop: 50,
-        marginBottom: 50,
-        marginHorizontal: 80,
-        height: 45,
-        borderRadius: 3,
-        backgroundColor: Colors.completeColor,
-        alignItems: 'center',
-        justifyContent: 'center'
-      },
-      buttonTitle: {
-        fontSize: 18,
-        color: '#fff'
-      }
-  });
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {renderAccordions(filteredExercises(categoryIDs))}
+        <TouchableOpacity style={styles.endButton} onPress={() => { navigation.navigate('Home') }}>
+          <Text style={styles.buttonTitle}>Finish</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+}
+
+const renderAccordions = (data) => {
+  const accordions = [];
+  for (let item of data) {
+    accordions.push(
+      <Accordion
+        key={item.id}
+        title={item.title}
+        description={item.description}
+        imagePath={item.imagePath}
+      />
+    )
+  }
+  return accordions;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: Colors.backgroundColor
+  },
+  endButton: {
+    marginTop: 50,
+    marginBottom: 50,
+    marginHorizontal: 80,
+    height: 45,
+    borderRadius: 3,
+    backgroundColor: Colors.completeColor,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonTitle: {
+    fontSize: 18,
+    color: '#fff'
+  }
+});
