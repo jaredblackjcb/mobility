a755141e68e462f0d734303e48f5091546030a24
Jared Black <jaredblackjcb@gmail.com>
2022-04-09 08:01:08 -0500

    Add repetitions and duration to exercise accordion

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,8 +1,10 @@
 export default class Exercise {
-    constructor(id, categoryIds, title, description, thumbnailPath, videoPath) {
+    constructor(id, categoryIds, title, repetitions, duration, description, thumbnailPath, videoPath) {
         this.id = id;
         this.categoryIds = categoryIds;
         this.title = title;
+        this.repetitions = repetitions;
+        this.duration = duration;
         this.description = description;
         this.thumbnailPath = thumbnailPath;
         this.videoPath = videoPath;
